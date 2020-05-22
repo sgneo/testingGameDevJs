f20fbf30d1485a81bd7ee21533ea4232ae22dd8d
George Stan <sgneo93@gmail.com>
2020-05-22 14:49:57 +0300

    Implementing delta time

--- src/game.js
+++ src/game.js
@@ -2,19 +2,30 @@ import Screen from "./screen"
 
 
 export default class Game {
-  constructor(width, height) {
+  constructor(width, height, fps) {
     this.screen = new Screen(width, height)
+    this.fps = 60;
+    this.currentFrameNum = 0;
+    this.lastUpdated = Date.now();
   }
 
   init() {
     this.screen.init()
   }
 
-  draw() {
-    console.log("DRAWING")
+  draw(deltaTime) {
+
   }
   update() {
-    this.draw()
+    const now = Date.now();
+    let deltaTime = (now - this.lastUpdated) / 1000;
+
+    if (deltaTime > 1/this.fps) {
+      this.lastUpdated = Date.now();
+      this.draw(deltaTime)
+      this.currentFrameNum++;
+    }
+
     requestAnimationFrame(this.update.bind(this))
   }
 }
